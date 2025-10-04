651685f0525f62b8f08197c98e2ce79e69df8d2d
Jitara <ursuaalejandro5@gmail.com>
2025-10-03 19:55:26 -0500

    Primer Cmmit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Login from './Forms/Login'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Login></Login>
+      </div>
+    </>
+  )
+}
+
+export default App
